ef3548f4fd0d141402cd52793b8b5f6b9ea3a81e
Alex Correa <alexxx.nada@gmail.com>
2021-10-28 15:24:38 -0300

    Formulario

---
+++ app.js
@@ -0,0 +1,35 @@
+console.log('Hello World!')
+
+// variaveis 
+let nome = "Usuario teste"
+let endereco = "Rua Tal tal tal"
+let data = "28/10/2021"
+let descricao = ""
+let foto_do_local
+let coordenada_x
+let coordenada_y
+let coordenadas
+let protocolo
+
+
+
+function Formulario() {
+
+    
+    console.log(nome)
+
+}
+
+
+    
+function ExibeFormulario() {
+
+}
+
+
+Formulario()
+//ExibeFormulario()
+
+
+
+
