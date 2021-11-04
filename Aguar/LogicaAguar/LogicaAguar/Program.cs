84ab5f74c5077ecde1022b0b666a580688e9a5d3
ArthurB95 <55153181+ArthurB95@users.noreply.github.com>
2021-11-04 12:58:51 -0300

    Add files via upload

---
+++ Aguar/LogicaAguar/LogicaAguar/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LogicaAguar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //LISTA DE USUÁRIOS
+            List<Usuario> lista = new List<Usuario>();
+
+            lista.Add(new Usuario("Arthur Bicalho", "arthurbic@Hotmail.com", "Rua Ouro Preto 1410, Santo Agostinho", "Belo Horizonte", "Minas Gerais", " 04/11/2021 12:42PM", "Cano estourado"));
+
+            foreach(Usuario item in lista)
+            {
+                Console.WriteLine(" Nome: " + item.Nome + "\n E-mail: " + item.Email + "\n Endereço: " + item.Endereco + "\n Cidade: " + item.Cidade + "\n Estado: " + item.Estado + "\n Data: " + item.Data);
+            }
+
+        }
+    }
+}
---
+++ Aguar/LogicaAguar/LogicaAguar/Usuario.cs
@@ -0,0 +1,33 @@
+﻿
+namespace LogicaAguar
+{
+    class Usuario
+    {
+        //ATRIBUTOS DO USUÁRIO
+        public string Nome { get; set; }
+
+        public string Email { get; set; }
+
+        public string Endereco { get; set; }
+
+        public string Cidade { get; set; }
+
+        public string Estado { get; set; }
+
+        public string Data { get; set; }
+
+        public string Tipo { get; set; }
+
+        //CONSTRUTOR DO USUÁRIO
+        public Usuario (string nome, string email, string endereco, string cidade, string estado, string data, string tipo)
+        {
+            Nome = nome;
+            Email = email;
+            Endereco = endereco;
+            Cidade = cidade;
+            Estado = estado;
+            Data = data;
+            Tipo = tipo;
+        }
+    }
+}
